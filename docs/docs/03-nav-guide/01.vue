1c9a05a45f075bc3350db603ae9a7846d1a07d6c
yangzhou <yearrow@qq.com>
2025-12-02 15:10:17 +0800

    feat(nav-guide): 添加导航组件示例和文档修改
    - 新增docs导航组件01.vue示例，展示基础数据和跳转功能
    - 在01-navigation.mdx中补充vue示例代码，优化语法格式
    - 修正文档表格对齐，调整属性说明的空格一致性
    - 添加tool插槽示例，展示导航栏工具按钮使用方法
    - 优化源码示例的代码块语言声明，提升可读性和预览效果

---
+++ docs/docs/03-nav-guide/01.vue
@@ -0,0 +1,18 @@
+<template>
+  <Navigation :routers="navData"></Navigation>
+</template>
+<script lang="ts" setup>
+import ElementPro from '@cs/element-pro';
+import { ElBreadcrumb, ElBreadcrumbItem } from 'element-plus';
+import { ref } from 'vue';
+
+const { Navigation } = ElementPro;
+console.log(Navigation);
+
+const navData = ref([
+  { title: '基础数据' },
+  { title: '收料登记', to: '/navigation' },
+  { title: '单据详情' },
+]);
+
+</script>
