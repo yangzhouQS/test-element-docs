cf293cddbfd2c26b04acf6fe9dee8ef0259b3882
yangzhou <yearrow@qq.com>
2025-12-02 11:05:50 +0800

    docs(icon): 优化图标组件示例和文档结构
    - 添加基于 element-plus 图标的新 Vue 组件示例 04/01.vue 和 04/02.vue
    - 调整 04-icon.mdx 文档，使用 vue 代码块格式引入并展示组件示例
    - 统一图标导入方式，提升示例代码规范性和可读性
    - 用 iframe-preview 特性增强示例预览体验

---
+++ docs/rules/03-base-element/04/01.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-button type="info" plain :icon="Search">查询</el-button>
+</template>
+
+<script lang="ts" setup>
+import { ElButton } from 'element-plus';
+import { Search } from '@element-plus/icons-vue';
+</script>
+
+<style scoped>
+
+</style>
---
+++ docs/rules/03-base-element/04/02.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-icon :size="20" color="red">
+    <Delete/>
+  </el-icon>
+  <el-icon :size="20" class="is-loading">
+    <Loading/>
+  </el-icon>
+  <el-icon :size="20" class="el-icon--left">
+    <Message/>
+  </el-icon>
+  <el-icon :size="20" class="el-icon--right">
+    <Search/>
+  </el-icon>
+</template>
+<script lang="ts" setup>
+import { ElIcon } from 'element-plus';
+import {
+  Delete,
+  Loading,
+  Message,
+  Search,
+} from '@element-plus/icons-vue';
+</script>
